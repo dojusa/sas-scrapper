423563439cffe99de6a94a67bf7b76e8b3e12e21
Jusabe Guedes <jusabe.09@gmail.com>
2022-11-20 20:52:43 +0100

    add category

--- physical_stores.go
+++ physical_stores.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Page struct {
@@ -15,6 +16,7 @@ type Shop struct {
 	Name        string `json:"name_common"`
 	Description string `json:"merchant_sas.description"`
 	RewardRate  string `json:"purchase_reward_rate"`
+	CategoryID  int    `json:"primary_category_id"`
 
 	// Address
 	Address string `json:"address"`
@@ -26,8 +28,35 @@ type Shop struct {
 	Visa       string `json:"merchant_visa_vlps.status"`
 }
 
+func (s Shop) Category() string {
+	switch s.CategoryID {
+	case 1:
+		return "Hotel/Cruise"
+	case 2:
+		return "Health/Wellness"
+	case 3:
+		return "Event"
+	case 4:
+		return "Sport/Leisure"
+	case 5:
+		return "Home/Interiors"
+	case 6:
+		return "Auto/Fuel"
+	case 8:
+		return "Clothes/Fashion"
+	case 9:
+		return "Grocery"
+	case 11:
+		return "Restaurant/Bar/Café"
+	case 12:
+		return "Other"
+	default:
+		return strconv.Itoa(s.CategoryID)
+	}
+}
+
 func (s Shop) AsCSVRow() []string {
-	return []string{s.Name, s.RewardRate, s.Amex, s.MasterCard, s.Visa, s.Description}
+	return []string{s.Name, s.RewardRate, s.Category(), s.Amex, s.MasterCard, s.Visa, s.Description}
 }
 
 func RunPhysicalStores() {
@@ -62,6 +91,6 @@ func RunPhysicalStores() {
 
 	fmt.Printf("Found %d shops\n", len(shops))
 
-	head := []string{"Name", "RewardRate", "Amex", "MasterCard", "Visa", "Description"}
+	head := []string{"Name", "RewardRate", "Category", "Amex", "MasterCard", "Visa", "Description"}
 	WriteCSV("output/physical_stores.csv", head, shops)
 }
